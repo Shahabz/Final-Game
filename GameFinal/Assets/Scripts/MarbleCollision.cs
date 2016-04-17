941566a489614cd7870e71e457ba7b64c4132095
rotem <r.shaul9@yahoo.com>
2016-04-16 23:09:57 -0700

    started new game

---
+++ GameFinal/Assets/Scripts/MarbleCollision.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class MarbleCollision : MonoBehaviour
+{
+	public UnityEvent onMarbleCollisionBlock;
+	public UnityEvent onMarbleCollisionDestination;
+	public UnityEvent winLevel;
+
+	void OnCollisionEnter2D (Collision2D other)
+	{
+		
+		if (other.gameObject.tag == "block") {
+				onMarbleCollisionBlock.Invoke ();
+				//Instantiate (blood, new Vector2 (transform.position.x, transform.position.y), Quaternion.identity);
+
+		}
+
+		if (other.gameObject.tag == "destination") {
+			onMarbleCollisionDestination.Invoke ();
+		}
+			
+	}
+
+
+		
+}
+
---
+++ GameFinal/Assets/Scripts/MarbleModel.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class MarbleModel : MonoBehaviour
+{
+
+	public GameObject marble;
+
+	void Start ()
+	{
+		marble.transform.position = new Vector3 (-2f, -2f, 0f);
+	}
+
+
+
+	void Update ()
+	{
+		
+	}
+		
+	public void HandleEnemyCollision ()
+	{
+		marble.transform.rotation = Quaternion.Euler (marble.transform.rotation.x, marble.transform.rotation.y, 50f);
+	}
+		
+}
