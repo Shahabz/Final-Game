8c7a14156e48f0983b1bf8a0181f7432c2f7f2eb
rotem <r.shaul9@yahoo.com>
2016-05-01 01:35:47 +0300

    added main menu

---
+++ GameFinal/Assets/Scripts/MenuModel.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuModel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void HandleClickedPlay ()
+	{
+		//Application.LoadLevel ("LevelMenu");
+		Application.LoadLevel ("Level1");
+	}
+
+	public void HandleClickedAbout ()
+	{
+		//Application.LoadLevel ("About");
+	}
+
+	public void HandleClickedHelp ()
+	{
+		Application.LoadLevel ("Help");
+	}
+
+	public void HandleClickedBackToMainMenu()
+	{
+		Application.LoadLevel ("StartMenu");
+	}
+}
